769fda64ac94db806469bd95186de2ba58abda83
iftruedo <jere.ahkiola@gmail.com>
2024-02-23 14:00:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+
+app.use(express.json())
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(n => n.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/notes/', (request, response) => {
+  //console.log(request)
+  console.log(request.body)
+  //console.log(request)
+  notes = notes.concat({...request.body, "id": 99})
+  response.json(request.body)
+  response.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
